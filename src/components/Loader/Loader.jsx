14de7b39ab4bf8425d64dc651bd86987adf95fa2
Oksana Repchak <oksa.repchak@gmail.com>
2020-12-23 20:14:53 +0200

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
 import Loader from 'react-loader-spinner';
 import s from './Loader.module.css';
 
-export default class Spinner extends Component {
-  render() {
-    return (
-      <Loader
-        className={s.Spiner}
-        type="Oval"
-        color="#00BFFF"
-        height={100}
-        width={100}
-        timeout={3000} //3 secs
-      />
-    );
-  }
+export default function Spinner() {
+  return (
+    <Loader
+      className={s.Spiner}
+      type="Oval"
+      color="#00BFFF"
+      height={100}
+      width={100}
+      timeout={3000} //3 secs
+    />
+  );
 }
